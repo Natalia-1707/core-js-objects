dfb21d05f2790ab0c5c44ca493af1dcc881606b2
natalia-1707 <nataliatsepoukhowa@gmail.com>
2025-02-27 02:31:20 +0300

    partly make tasks

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const obj2 = {};
+  return Object.assign(obj2, obj);
 }
 
 /**
@@ -32,8 +33,14 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      result[key] = (result[key] || 0) + value;
+    });
+  });
+  return result;
 }
 
 /**
@@ -49,8 +56,15 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  let obj2 = {};
+  obj2 = { ...obj };
+  for (let i = 0; i < keys.length; i += 1) {
+    if (Object.keys(obj2).includes(keys[i])) {
+      delete obj2[keys[i]];
+    }
+  }
+  return obj2;
 }
 
 /**
@@ -65,8 +79,21 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const obj1Arr = Object.entries(obj1);
+  const obj2Arr = Object.entries(obj2);
+  if (obj1Arr.length !== obj2Arr.length) {
+    return false;
+  }
+  for (let i = 0; i < obj1Arr.length; i += 1) {
+    if (obj1Arr[i][0] !== obj2Arr[i][0]) {
+      return false;
+    }
+    if (obj1Arr[i][1] !== obj2Arr[i][1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -80,8 +107,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -100,8 +127,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -114,8 +141,19 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+  const resultLength = Object.values(lettersObject).flat().length;
+  for (let j = 0; j < resultLength; j += 1) {
+    result.push('');
+  }
+  const array = Object.entries(lettersObject);
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array[i][1].length; j += 1) {
+      result.splice(array[i][1][j], 1, array[i][0]);
+    }
+  }
+  return result.join('');
 }
 
 /**
@@ -132,8 +170,30 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let bill25 = 0;
+  let bill50 = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] === 25) {
+      bill25 += 1;
+    } else if (queue[i] === 50) {
+      if (bill25 === 0) {
+        return false;
+      }
+      bill25 -= 1;
+      bill50 += 1;
+    } else if (queue[i] === 100) {
+      if (bill25 > 0 && bill50 > 0) {
+        bill50 -= 1;
+        bill25 -= 1;
+      } else if (bill25 >= 3) {
+        bill25 -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
